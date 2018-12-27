b12e8fc42a7b71a8f8467bca7c2059a4ef654981
sheikhsuleman123 <mssheikh333@gmail.com>
2018-12-27 13:46:32 +0500

    complete the auto search styling

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import './Loading.css'
 
-const Loading = () => {
-    return <div className="Loading">
+import PropTypes from 'prop-types';
+
+import './Loading.css';
+
+const Loading = (props) => {
+   
+   const {width, height } = props;
+   
+   return <div className="Loading" style={{width, height}}>
 
     </div>
 }
+
+Loading.defaultProps = {
+    width: '28px',
+    height: '28px',
+};
+Loading.PropTypes = {
+    width: PropTypes.string,
+    height: PropTypes.string,
+}
+
+
 
 export default Loading;
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,29 +1,67 @@
-import React, { Component } from 'react'
-import './search.css'
+import React, { Component } from 'react';
+import {API_URL} from '../../config';
+import { handleResponce } from '../../helpers';
+import Loading from '../Loading';
+
+import './search.css';
 
  class Search extends Component {
      constructor(){
          super();
          this.state = {
              searchQuery: '',
+             loading: false,
          };
          this.handlChange = this.handlChange.bind(this);
      }
      handlChange(event) {
-         const inputValue = event.target.value;
+         const searchQuery = event.target.value;
 
-         this.setState({searchQuery: inputValue});
+         this.setState({searchQuery });
 
-         console.log(this.state);
+         if(!searchQuery) {
+             return '';
+         }
+
+         this.setState({loading:true});
+
+         fetch(`${API_URL}/autocomplete?searchQuery=${searchQuery}`)
+         .then(handleResponce)
+         .then((result) => {
+             console.log(result)
+
+             this.setState({loading:false});
+
+
+         });
      }
 
+
   render() {
+
+    const {loading } = this.state;
+
     return (
-      <div>
-        <form>
-            <input onChange={this.handlChange} />
+      <div className="Search">
+        <span className="Search-icon">
+
+        </span>
+
+            <input
+            className="Search-input"
+            type="text"
+            placeholder="Currency-name"
+            onChange={this.handlChange} 
+            />
+
+            {loading &&
+        <div className="Search-loading">
+            <Loading 
+            height='12px'
+            width='12px'
             
-        </form>
+            />
+        </div>}
       </div>
     )
   }
